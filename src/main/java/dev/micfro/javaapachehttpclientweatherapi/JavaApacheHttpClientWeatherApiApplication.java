afdfa22ff175a4cfdbd54dd6f30c4aa4666e9717
micfro <michalfrost71@gmail.com>
2024-04-17 11:31:23 +0200

    Initial commit

---
+++ src/main/java/dev/micfro/javaapachehttpclientweatherapi/JavaApacheHttpClientWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package dev.micfro.javaapachehttpclientweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApacheHttpClientWeatherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApacheHttpClientWeatherApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/micfro/javaapachehttpclientweatherapi/JavaApacheHttpClientWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.micfro.javaapachehttpclientweatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApacheHttpClientWeatherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
